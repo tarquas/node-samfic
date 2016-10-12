97dd285dc56c993490d606da597a9c916d95d0b5
Taras Mykhailovych <tarquas@gmail.com>
2016-10-12 20:47:04 +0300

    add npm start script

--- index.js
+++ index.js
@@ -6,8 +6,3 @@ const symbol = require('./src/symbol');
 Frame.Co = Co;
 Frame.Funnel = Funnel;
 Frame.symbol = symbol;
-
-if (require.main === module) {
-  const Status = require('./status'); // eslint-disable-line
-  Status.forceMain = true;
-}
